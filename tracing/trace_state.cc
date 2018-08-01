bbfef2cc0dcdefc02e44e8fc774975f73c9d3345
Vlad Zolotarov <vladz@scylladb.com>
2018-08-01 12:05:46 -0400

    tracing: store a query response size in session's "parameters" map
    Store the size of uncompressed response size.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- tracing/trace_state.cc
+++ tracing/trace_state.cc
@@ -62,6 +62,7 @@ struct trace_state::params_values {
     std::experimental::optional<int32_t> page_size;
     std::vector<prepared_checked_weak_ptr> prepared_statements;
     size_t request_size = 0;
+    size_t response_size = 0;
 };
 
 trace_state::params_values* trace_state::params_ptr::get_ptr_safe() {
@@ -97,6 +98,10 @@ void trace_state::set_page_size(int32_t val) {
 
 void trace_state::set_request_size(size_t s) noexcept {
     _params_ptr->request_size = s;
+}
+
+void trace_state::set_response_size(size_t s) noexcept {
+    _params_ptr->response_size = s;
 }
 
 void trace_state::add_query(const sstring& val) {
@@ -172,6 +177,7 @@ void trace_state::build_parameters_map(const cql3::query_options* prepared_optio
     }
 
     params_map.emplace("request_size", seastar::format("{:d}", vals.request_size));
+    params_map.emplace("response_size", seastar::format("{:d}", vals.response_size));
 }
 
 void trace_state::build_parameters_map_for_one_prepared(const prepared_checked_weak_ptr& prepared_ptr, const cql3::query_options& options, const sstring& param_name_prefix) {
