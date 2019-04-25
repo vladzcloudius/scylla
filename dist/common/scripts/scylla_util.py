scylladb/scylladb

commit c3dae6673f53b0e838d98c19094a138d528fb5b9

dist/common/scripts: use out() to run perftune.py
perftune.py executes hwloc-calc, the command is now provided as
relocatable binary, placed under /opt/scylladb/bin.
So we need to add the directory to PATH when calling
subprocess.check_output(), but our utility function already do that,
switch to it.

Fixes #4443

Signed-off-by: Takuya ASADA <syuu@scylladb.com>
Message-Id: <20190418124345.24973-1-syuu@scylladb.com>

dist/common/scripts/scylla_util.py
@@ -404,7 +404,7 @@ def colorprint(msg, **kwargs):
 
 def get_mode_cpuset(nic, mode):
     try:
-        mode_cpu_mask = subprocess.check_output(['/usr/lib/scylla/perftune.py', '--tune', 'net', '--nic', nic, '--mode', mode, '--get-cpu-mask'], stderr=subprocess.DEVNULL).strip().decode('utf-8')
+        mode_cpu_mask = out('/usr/lib/scylla/perftune.py --tune net --nic {} --mode {} --get-cpu-mask'.format(nic, mode))
         return hex2list(mode_cpu_mask)
     except subprocess.CalledProcessError:
         return '-1'

vladzcloudius/scylla

commit 21cd50884c3e65743c553f099e5b70febb54d0b8

dist/common/script: conceal perftune.py error messages when calling it in order to get mode's CPU mask #2
This patch makes perftune.py call possible from relocatable package
installation without breaking the original behavior by simply passing the
"scylla_env" as an "env" parameter to the original subprocess.check_output()
call.

This patch fixes the regression introduced by the commit c3dae6673f53b0e838d98c19094a138d528fb5b9
which reverted the fix from cb7215c56e43c3842aac3b6df9a15cdcfb654233.

Fixes #4211
Fixes #4443

Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

dist/common/scripts/scylla_util.py
@@ -404,7 +404,7 @@ def colorprint(msg, **kwargs):
 
 def get_mode_cpuset(nic, mode):
     try:
-        mode_cpu_mask = out('/usr/lib/scylla/perftune.py --tune net --nic {} --mode {} --get-cpu-mask'.format(nic, mode))
+        mode_cpu_mask = subprocess.check_output(['/usr/lib/scylla/perftune.py', '--tune', 'net', '--nic', nic, '--mode', mode, '--get-cpu-mask'], stderr=subprocess.DEVNULL, env=scylla_env).strip().decode('utf-8')
         return hex2list(mode_cpu_mask)
     except subprocess.CalledProcessError:
         return '-1'