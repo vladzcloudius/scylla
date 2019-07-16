scylladb/scylladb

commit 4ef940169f09a2b10484cd40b1a59d2cbec6f525

Replace use of "ipv4_addr" with socket_address
Allows the various sockets to use ipv6 address binding if so configured.

locator/ec2_snitch.cc
@@ -63,7 +63,7 @@ future<> ec2_snitch::start() {
 }
 
 future<sstring> ec2_snitch::aws_api_call(sstring addr, sstring cmd) {
-    return engine().net().connect(make_ipv4_address(ipv4_addr{addr}))
+    return engine().net().connect(socket_address(inet_address{addr}))
     .then([this, addr, cmd] (connected_socket fd) {
         _sd = std::move(fd);
         _in = std::move(_sd.input());

api/messaging_service.cc
@@ -76,7 +76,7 @@ future_json_function get_server_getter(std::function<uint64_t(const rpc::stats&)
         auto get_shard_map = [f](messaging_service& ms) {
             std::unordered_map<gms::inet_address, unsigned long> map;
             ms.foreach_server_connection_stats([&map, f] (const rpc::client_info& info, const rpc::stats& stats) mutable {
-                map[gms::inet_address(net::ipv4_address(info.addr))] = f(stats);
+                map[gms::inet_address(info.addr.addr())] = f(stats);
             });
             return map;
         };

db/system_keyspace.cc
@@ -1605,7 +1605,6 @@ future<> update_peer_info(gms::inet_address ep, sstring column_name, Value value
 // sets are not needed, since tokens are updated by another method
 template future<> update_peer_info<sstring>(gms::inet_address ep, sstring column_name, sstring);
 template future<> update_peer_info<utils::UUID>(gms::inet_address ep, sstring column_name, utils::UUID);
-template future<> update_peer_info<net::ipv4_address>(gms::inet_address ep, sstring column_name, net::ipv4_address);
 template future<> update_peer_info<net::inet_address>(gms::inet_address ep, sstring column_name, net::inet_address);
 
 future<> update_hints_dropped(gms::inet_address ep, utils::UUID time_period, int value) {

locator/gce_snitch.cc
@@ -111,7 +111,7 @@ future<sstring> gce_snitch::gce_api_call(sstring addr, sstring cmd) {
         using namespace boost::algorithm;
 
         net::inet_address a = seastar::net::dns::resolve_name(addr, net::inet_address::family::INET).get0();
-        connected_socket sd(connect(make_ipv4_address(ipv4_addr(a, 80))).get0());
+        connected_socket sd(connect(socket_address(a, 80)).get0());
         input_stream<char> in(sd.input());
         output_stream<char> out(sd.output());
         sstring zone_req(seastar::format("GET {} HTTP/1.1\r\nHost: metadata\r\nMetadata-Flavor: Google\r\n\r\n", cmd));

main.cc
@@ -535,7 +535,7 @@ int main(int ac, char** av) {
                 }));
                 prometheus::start(prometheus_server, pctx);
                 with_scheduling_group(maintenance_scheduling_group, [&] {
-                  return prometheus_server.listen(ipv4_addr{prom_addr.addr_list.front(), pport}).handle_exception([pport, &cfg] (auto ep) {
+                  return prometheus_server.listen(socket_address{prom_addr.addr_list.front(), pport}).handle_exception([pport, &cfg] (auto ep) {
                     startlog.error("Could not start Prometheus API server on {}:{}: {}", cfg->prometheus_address(), pport, ep);
                     return make_exception_future<>(ep);
                   });
@@ -618,7 +618,7 @@ int main(int ac, char** av) {
             ctx.http_server.start("API").get();
             api::set_server_init(ctx).get();
             with_scheduling_group(maintenance_scheduling_group, [&] {
-                return ctx.http_server.listen(ipv4_addr{ip, api_port});
+                return ctx.http_server.listen(socket_address{ip, api_port});
             }).get();
             startlog.info("Scylla API server listening on {}:{} ...", api_address, api_port);
             static sharded<auth::service> auth_service;

message/messaging_service.cc
@@ -146,10 +146,10 @@ class messaging_service::rpc_protocol_client_wrapper {
     std::unique_ptr<rpc_protocol::client> _p;
     ::shared_ptr<seastar::tls::server_credentials> _credentials;
 public:
-    rpc_protocol_client_wrapper(rpc_protocol& proto, rpc::client_options opts, ipv4_addr addr, ipv4_addr local = ipv4_addr())
+    rpc_protocol_client_wrapper(rpc_protocol& proto, rpc::client_options opts, socket_address addr, socket_address local = {})
             : _p(std::make_unique<rpc_protocol::client>(proto, std::move(opts), addr, local)) {
     }
-    rpc_protocol_client_wrapper(rpc_protocol& proto, rpc::client_options opts, ipv4_addr addr, ipv4_addr local, ::shared_ptr<seastar::tls::server_credentials> c)
+    rpc_protocol_client_wrapper(rpc_protocol& proto, rpc::client_options opts, socket_address addr, socket_address local, ::shared_ptr<seastar::tls::server_credentials> c)
             : _p(std::make_unique<rpc_protocol::client>(proto, std::move(opts), seastar::tls::socket(c), addr, local))
             , _credentials(c)
     {}
@@ -296,7 +296,7 @@ void messaging_service::start_listen() {
     auto limits = rpc_resource_limits(_mcfg.rpc_memory_limit);
     if (!_server[0]) {
         auto listen = [&] (const gms::inet_address& a) {
-            auto addr = ipv4_addr{a.raw_addr(), _port};
+            auto addr = socket_address{a, _port};
             return std::unique_ptr<rpc_protocol_server_wrapper>(new rpc_protocol_server_wrapper(*_rpc,
                     so, addr, limits));
         };
@@ -316,7 +316,7 @@ void messaging_service::start_listen() {
                 listen_options lo;
                 lo.reuse_address = true;
                 lo.lba =  server_socket::load_balancing_algorithm::port;
-                auto addr = make_ipv4_address(ipv4_addr{a.raw_addr(), _ssl_port});
+                auto addr = socket_address{a, _ssl_port};
                 return std::make_unique<rpc_protocol_server_wrapper>(*_rpc,
                         so, seastar::tls::listen(_credentials, addr, lo), limits);
             }());
@@ -601,7 +601,7 @@ shared_ptr<messaging_service::rpc_protocol_client_wrapper> messaging_service::ge
         return true;
     }();
 
-    auto remote_addr = ipv4_addr(get_preferred_ip(id.addr).raw_addr(), must_encrypt ? _ssl_port : _port);
+    auto remote_addr = socket_address(get_preferred_ip(id.addr).raw_addr(), must_encrypt ? _ssl_port : _port);
 
     rpc::client_options opts;
     // send keepalive messages each minute if connection is idle, drop connection after 10 failures
@@ -613,7 +613,7 @@ shared_ptr<messaging_service::rpc_protocol_client_wrapper> messaging_service::ge
 
     auto client = must_encrypt ?
                     ::make_shared<rpc_protocol_client_wrapper>(*_rpc, std::move(opts),
-                                    remote_addr, ipv4_addr(), _credentials) :
+                                    remote_addr, socket_address(), _credentials) :
                     ::make_shared<rpc_protocol_client_wrapper>(*_rpc, std::move(opts),
                                     remote_addr);
 

service/client_state.hh
@@ -179,7 +179,6 @@ public:
             , _cpu_of_origin(engine().cpu_id())
             , _is_internal(true)
             , _is_thrift(false)
-            , _remote_address(ipv4_addr())
     {}
 
     ///

service/storage_service.cc
@@ -2192,7 +2192,7 @@ future<> storage_service::start_rpc_server() {
                 //engine().at_exit([tserver] {
                 //    return tserver->stop();
                 //});
-                return tserver->invoke_on_all(&thrift_server::listen, ipv4_addr{ip, port}, keepalive);
+                return tserver->invoke_on_all(&thrift_server::listen, socket_address{ip, port}, keepalive);
             });
         }).then([addr, port] {
             slogger.info("Thrift server listening on {}:{} ...", addr, port);
@@ -2251,11 +2251,11 @@ future<> storage_service::start_native_transport() {
                 auto f = make_ready_future();
 
                 struct listen_cfg {
-                    ipv4_addr addr;
+                    socket_address addr;
                     std::shared_ptr<seastar::tls::credentials_builder> cred;
                 };
 
-                std::vector<listen_cfg> configs({ { ipv4_addr{ip, cfg.native_transport_port()} }});
+                std::vector<listen_cfg> configs({ { socket_address{ip, cfg.native_transport_port()} }});
 
                 // main should have made sure values are clean and neatish
                 if (ceo.at("enabled") == "true") {
@@ -2280,7 +2280,7 @@ future<> storage_service::start_native_transport() {
                     slogger.info("Enabling encrypted CQL connections between client and server");
 
                     if (cfg.native_transport_port_ssl.is_set() && cfg.native_transport_port_ssl() != cfg.native_transport_port()) {
-                        configs.emplace_back(listen_cfg{ipv4_addr{ip, cfg.native_transport_port_ssl()}, std::move(cred)});
+                        configs.emplace_back(listen_cfg{{ip, cfg.native_transport_port_ssl()}, std::move(cred)});
                     } else {
                         configs.back().cred = std::move(cred);
                     }

vladzcloudius/scylla

commit 476e79d9298fcc0605b8bda8d9e0886e3e746bf8

ec2_snitch: properly build the AWS meta server address
This patch fixes the regression introduced by
4ef940169f09a2b10484cd40b1a59d2cbec6f525.

Explicity pass the port number of the AWS metadata server API
when creating a corresponding socket.

Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

locator/ec2_multi_region_snitch.cc
@@ -55,7 +55,7 @@ future<> ec2_multi_region_snitch::start() {
             inet_address local_public_address;
 
             try {
-                pub_addr = aws_api_call(AWS_QUERY_SERVER_ADDR, PUBLIC_IP_QUERY_REQ).get0();
+                pub_addr = aws_api_call(AWS_QUERY_SERVER_ADDR, AWS_QUERY_SERVER_PORT, PUBLIC_IP_QUERY_REQ).get0();
                 local_public_address = inet_address(pub_addr);
             } catch (...) {
                 std::throw_with_nested(exceptions::configuration_exception("Failed to get a Public IP. Public IP is a requirement for Ec2MultiRegionSnitch. Consider using a different snitch if your instance doesn't have it"));
@@ -71,7 +71,7 @@ future<> ec2_multi_region_snitch::start() {
             utils::fb_utilities::set_broadcast_address(local_public_address);
             utils::fb_utilities::set_broadcast_rpc_address(local_public_address);
 
-            sstring priv_addr = aws_api_call(AWS_QUERY_SERVER_ADDR, PRIVATE_IP_QUERY_REQ).get0();
+            sstring priv_addr = aws_api_call(AWS_QUERY_SERVER_ADDR, AWS_QUERY_SERVER_PORT, PRIVATE_IP_QUERY_REQ).get0();
             _local_private_address = priv_addr;
 
             //

locator/ec2_snitch.cc
@@ -17,7 +17,7 @@ future<> ec2_snitch::load_config() {
     using namespace boost::algorithm;
 
     if (engine().cpu_id() == io_cpu_id()) {
-        return aws_api_call(AWS_QUERY_SERVER_ADDR, ZONE_NAME_QUERY_REQ).then([this](sstring az){
+        return aws_api_call(AWS_QUERY_SERVER_ADDR, AWS_QUERY_SERVER_PORT, ZONE_NAME_QUERY_REQ).then([this](sstring az){
             assert(az.size());
 
             std::vector<std::string> splits;
@@ -62,8 +62,8 @@ future<> ec2_snitch::start() {
     });
 }
 
-future<sstring> ec2_snitch::aws_api_call(sstring addr, sstring cmd) {
-    return engine().net().connect(socket_address(inet_address{addr}))
+future<sstring> ec2_snitch::aws_api_call(sstring addr, uint16_t port, sstring cmd) {
+    return engine().net().connect(socket_address(inet_address{addr}, port))
     .then([this, addr, cmd] (connected_socket fd) {
         _sd = std::move(fd);
         _in = std::move(_sd.input());

locator/ec2_snitch.hh
@@ -27,7 +27,8 @@ namespace locator {
 class ec2_snitch : public production_snitch_base {
 public:
     static constexpr const char* ZONE_NAME_QUERY_REQ = "/latest/meta-data/placement/availability-zone";
-    static constexpr const char* AWS_QUERY_SERVER_ADDR = "169.254.169.254:80";
+    static constexpr const char* AWS_QUERY_SERVER_ADDR = "169.254.169.254";
+    static constexpr uint16_t AWS_QUERY_SERVER_PORT = 80;
 
     ec2_snitch(const sstring& fname = "", unsigned io_cpu_id = 0);
     virtual future<> start() override;
@@ -36,7 +37,7 @@ public:
     }
 protected:
     future<> load_config();
-    future<sstring> aws_api_call(sstring addr, const sstring cmd);
+    future<sstring> aws_api_call(sstring addr, uint16_t port, const sstring cmd);
     future<sstring> read_property_file();
 private:
     connected_socket _sd;