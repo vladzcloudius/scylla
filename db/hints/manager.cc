7bb7862c9f846f47d92fd9bfb9ba2ac5b2073d53
Vlad Zolotarov <vladz@scylladb.com>
2019-08-12 14:35:51 -0400

    db::hints: cosmetics: identation
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- db/hints/manager.cc
+++ db/hints/manager.cc
@@ -120,7 +120,7 @@ future<> manager::stop() {
     return _draining_eps_gate.close().finally([this] {
         return parallel_for_each(_ep_managers, [] (auto& pair) {
                 return pair.second.stop();
-            }).finally([this] {
+        }).finally([this] {
             _ep_managers.clear();
             manager_logger.info("Stopped");
         }).discard_result();
