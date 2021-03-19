e9cb5b0c0cb81d294bb2bac459dc070ae9d73bf9
Ivan Prisyazhnyy <ivan@scylladb.com>
2021-03-19 17:22:13 +0200

    tracing: avoid copying a query string in a prepared statements tracing hot path
    To optimize code hot paths for tracing we are trying to remove dynamic
    allocations and memory copying as much as possible. One of the biggest
    contributor to the performance overhead in tracing is string formatting.
    
    The hottest code path is expected to be the execution of prepared
    statements. A prepared statement usually has a single query to trace.
    Usually prep statements are all compiled and cached. Thus we can omit
    copying and allocating memory of keeping the track of the related query.
    
    This patch changes the way trace state params tracks queries for cached
    prepared statement by reusing prepared_checked_weak_ptr. This allows to save 1 dyn
    allocation and copy on the hot path.
    
    Signed-off-by: Ivan Prisyazhnyy <ivan@scylladb.com>

--- transport/server.cc
+++ transport/server.cc
@@ -1004,7 +1004,6 @@ process_execute_internal(service::client_state& client_state, distributed<cql3::
         tracing::set_page_size(trace_state, options.get_page_size());
         tracing::set_consistency_level(trace_state, options.get_consistency());
         tracing::set_optional_serial_consistency_level(trace_state, options.get_serial_consistency());
-        tracing::add_query(trace_state, prepared->statement->raw_cql_statement);
         tracing::add_prepared_statement(trace_state, prepared);
 
         tracing::begin(trace_state, seastar::value_of([&id] { return seastar::format("Execute CQL3 prepared query [{}]", id); }),
